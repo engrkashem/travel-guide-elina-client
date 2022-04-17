5ae2687bbcd844cbdb5542d80fffae85c1410a46
engrkashem <kashemaust@gmail.com>
2022-04-17 11:06:51 +0600

    corousel added to the home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Slider from '../Slider/Slider';
 import './Home.css';
 
 const Home = () => {
     return (
         <div>
             <Services></Services>
+            <Slider></Slider>
         </div>
     );
 };
---
+++ src/Pages/Home/Slider/Slider.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button, Carousel } from 'react-bootstrap';
+import london from '../../../images/london.png';
+import america from '../../../images/america.png';
+import asia from '../../../images/asia.png';
+
+const Slider = () => {
+    return (
+        <div>
+            <Carousel fade>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src={london}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h1>LONDON</h1>
+                        <Button>Explore Europe</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                        className="d-block w-100"
+                        src={america}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h1>USA</h1>
+                        <Button>Explore America</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={asia}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h1>ASIA</h1>
+                        <Button>Explore ASIA</Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Slider;
