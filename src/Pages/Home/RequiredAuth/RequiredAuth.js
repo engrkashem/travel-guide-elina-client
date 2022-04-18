96cfb4201ac55c160777a10c4f55ce785993bd6c
engrkashem <kashemaust@gmail.com>
2022-04-18 09:04:14 +0600

    protected loading redirect to login issue solved

--- src/Pages/Home/RequiredAuth/RequiredAuth.js
+++ src/Pages/Home/RequiredAuth/RequiredAuth.js
@@ -1,12 +1,21 @@
 import React, { Children } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const RequiredAuth = ({ children }) => {
 
-    const [user] = useAuthState(auth)
+    const [user, loading] = useAuthState(auth)
     let location = useLocation();
+
+    if (loading) {
+        return <p>
+            <Spinner animation="border" variant="danger" />
+            <Spinner animation="border" variant="warning" />
+            <Spinner animation="border" variant="info" />
+        </p>
+    }
 
     if (!user) {
         return <Navigate to="/sign-in" state={{ from: location }} replace />;
